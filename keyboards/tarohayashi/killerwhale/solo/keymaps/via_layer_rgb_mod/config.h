0e61df8c76f72b78977e1631319491757a824cf2
doublebooking141 <doublebooking141@gmail.com>
2024-01-14 11:44:52 +0900

    migrate from tekitou git

---
+++ keyboards/tarohayashi/killerwhale/solo/keymaps/via_layer_rgb_mod/config.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Hayashi (@w_vwbw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 32
+#define RGBLIGHT_LIMIT_VAL 127

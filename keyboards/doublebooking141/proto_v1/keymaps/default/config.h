09232f48f0a27d3648b2bc079cd0ac7cea41ca66
doublebooking141 <doublebooking141@gmail.com>
2024-01-14 17:01:01 +0900

    一旦作業終了混乱している

--- keyboards/doublebooking141/proto_v1/keymaps/default/config.h
+++ keyboards/doublebooking141/proto_v1/keymaps/default/config.h
@@ -3,7 +3,15 @@
 
 #pragma once
 
+
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_LAYERS
 #define RGBLIGHT_MAX_LAYERS 32
 #define RGBLIGHT_LIMIT_VAL 127
+
+
+
+
+// #define JOYSTICK_BUTTON_COUNT 16
+// #define JOYSTICK_AXIS_COUNT 6
+// #define JOYSTICK_AXIS_RESOLUTION 10
--- keyboards/doublebooking141/proto_v1/rules.mk
+++ keyboards/doublebooking141/proto_v1/rules.mk
@@ -1,2 +1,3 @@
-# This file intentionally left blank
+# This file intentionally left blankl
+POINTING_DEVICE_DRIVER = analog_joystick
 SRC += analog.c
